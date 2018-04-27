d7fb67989557fe1acf7e562132f5b1d654ee6b01
Obinna Johnphill <obinna.johnphill@enta.net>
2018-04-27 13:23:09 +0100

    added facade cache

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -2,12 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Cache;
+
 use App\Articles;
 
 class ArticlesController extends Controller
 {
     public function index() {
-        $articles = Articles::all();
+
+        $time = 24*60;
+
+        $articles = Cache::remember('articles', $time, function() {
+            return Articles::all();
+        });
         return response()->json($articles);
     }
 }
